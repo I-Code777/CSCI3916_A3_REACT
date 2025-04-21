fb21828975d3f21e8cb6ed1d12e9a4d2d06bf738
I-Code777 <191649398+I-Code777@users.noreply.github.com>
2025-04-20 22:41:41 -0600

    Update moviedetail.js

--- CSC3916_REACT19-main/src/components/moviedetail.js
+++ CSC3916_REACT19-main/src/components/moviedetail.js
@@ -1,70 +1,69 @@
 import React, { useEffect } from 'react';
 import { fetchMovie } from '../actions/movieActions';
 import { useDispatch, useSelector } from 'react-redux';
-import { Card, ListGroup, ListGroupItem, Image } from 'react-bootstrap';
+import { Card, ListGroup, ListGroupItem, Image, Spinner } from 'react-bootstrap';
 import { BsStarFill } from 'react-icons/bs';
-import { useParams } from 'react-router-dom'; // Import useParams
+import { useParams } from 'react-router-dom';
 
 const MovieDetail = () => {
   const dispatch = useDispatch();
-  const { movieId } = useParams(); // Get movieId from URL parameters
-  const selectedMovie = useSelector(state => state.movie.selectedMovie);
-  const loading = useSelector(state => state.movie.loading); // Assuming you have a loading state in your reducer
-  const error = useSelector(state => state.movie.error); // Assuming you have an error state in your reducer
-
+  const { movieId } = useParams();
+  const { selectedMovie, loading, error } = useSelector(state => state.movie);
 
   useEffect(() => {
     dispatch(fetchMovie(movieId));
   }, [dispatch, movieId]);
 
-  const DetailInfo = () => {
-    if (loading) {
-      return <div>Loading....</div>;
-    }
-
-    if (error) {
-      return <div>Error: {error}</div>;
-    }
-
-    if (!selectedMovie) {
-      return <div>No movie data available.</div>;
-    }
-
-    return (
-      <Card className="bg-dark text-dark p-4 rounded">
-        <Card.Header>Movie Detail</Card.Header>
-        <Card.Body>
-          <Image className="image" src={selectedMovie.imageUrl} thumbnail />
-        </Card.Body>
-        <ListGroup>
-          <ListGroupItem>{selectedMovie.title}</ListGroupItem>
-          <ListGroupItem>
-            {selectedMovie.actors.map((actor, i) => (
-              <p key={i}>
-                <b>{actor.actorName}</b> {actor.characterName}
-              </p>
-            ))}
-          </ListGroupItem>
-          <ListGroupItem>
-            <h4>
-              <BsStarFill /> {selectedMovie.avgRating}
-            </h4>
-          </ListGroupItem>
-        </ListGroup>
-        <Card.Body>
-          {selectedMovie.reviews.map((review, i) => (
-            <p key={i}>
-              <b>{review.username}</b>&nbsp; {review.review} &nbsp; <BsStarFill />{' '}
-              {review.rating}
-            </p>
-          ))}
-        </Card.Body>
-      </Card>
-    );
-  };
-
-  return <DetailInfo />;
+  return <DetailInfo loading={loading} error={error} selectedMovie={selectedMovie} />;
 };
 
+const DetailInfo = ({ loading, error, selectedMovie }) => {
+  if (loading) {
+    return <Spinner animation="border" role="status"><span className="visually-hidden">Loading...</span></Spinner>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!selectedMovie) {
+    return <div>No movie data available.</div>;
+  }
+
+  return (
+    <Card className="bg-dark text-dark p-4 rounded">
+      <Card.Header>Movie Detail</Card.Header>
+      <Card.Body>
+        <Image
+          className="image"
+          src={selectedMovie.imageUrl || 'https://via.placeholder.com/300x450?text=No+Image'}
+          thumbnail
+        />
+      </Card.Body>
+      <ListGroup>
+        <ListGroupItem>{selectedMovie.title}</ListGroupItem>
+        <ListGroupItem>
+          {selectedMovie.actors && selectedMovie.actors.map((actor, i) => (
+            <p key={i}>
+              <b>{actor.actorName}</b> as {actor.characterName}
+            </p>
+          ))}
+        </ListGroupItem>
+        <ListGroupItem>
+          <h4>
+            <BsStarFill /> {selectedMovie.avgRating ? selectedMovie.avgRating.toFixed(1) : 'No ratings yet'}
+          </h4>
+        </ListGroupItem>
+      </ListGroup>
+      <Card.Body>
+        {selectedMovie.reviews && selectedMovie.reviews.map((review, i) => (
+          <p key={i}>
+            <b>{review.username}</b> {review.review} <BsStarFill /> {review.rating}
+          </p>
+        ))}
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default MovieDetail;
